f45a3ea0b1a126a94b0736d45201f29892b6e73b
Ritik Mishra <ritik.mishra314@gmail.com>
2018-01-25 23:40:09 -0600

    random empire names

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 from app import App
 from empire import Empire
@@ -12,18 +13,13 @@ if __name__ == '__main__':
 
     Empire.APP = app
 
-    romans = Empire("Roman", grid.rand_select())
+    names = None
+    with open("empire_names.txt") as f_name:
+        names = f_name.read().split("\n")
 
-    ottomans = Empire("Ottoman", grid.rand_select())
-
-    egyptians = Empire("Egyptian", grid.rand_select())
-
-    greek = Empire("Greek", grid.rand_select())
-
-    app.add_empire(romans)
-    app.add_empire(ottomans)
-    app.add_empire(egyptians)
-    app.add_empire(greek)
+    num_empires = 8
+    for _ in range(num_empires):
+        app.add_empire(Empire(random.choice(names), grid.rand_select()))
 
     for x in range(300):
         for y in range(300):
